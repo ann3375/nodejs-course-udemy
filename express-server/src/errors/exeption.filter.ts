c1e634c4d5a6ad195f20ea6830dd80a54708f739
Victor Kozykin <kozykin_victor_95@mail.ru>
2021-12-05 21:43:58 +0700

    feat: add controller

---
+++ express-server/src/errors/exeption.filter.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Response, Request } from "express";
+import { LoggerService } from "../logger/logger.service";
+import { iExeptionFilter } from "./exeption.filter.interface";
+import { HTTPError } from "./http-error.class";
+
+export class ExeptionFilter implements iExeptionFilter {
+  logger: LoggerService;
+
+  constructor(logger: LoggerService) {
+    this.logger = logger;
+  }
+
+  catch(
+    err: Error | HTTPError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (err instanceof HTTPError) {
+      this.logger.error(
+        `[${err.context}] Ошибка ${err.statusCode}: ${err.message}`
+      );
+      res.status(err.statusCode).send({ err: err.message });
+    } else {
+      this.logger.error(`${err.message}`);
+      res.status(500).send({ err: err.message });
+    }
+  }
+}
